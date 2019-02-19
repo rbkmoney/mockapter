4ced65da14bb36598adfd76a42db9cdcc55c6e77
Pospolita Nikita <nikita7asics@gmail.com>
2019-02-19 19:47:56 +0300

    creation (#1)

---
+++ src/main/java/com/rbkmoney/mockapter/MockapterApplication.java
@@ -0,0 +1,15 @@
+package com.rbkmoney.mockapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class MockapterApplication extends SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockapterApplication.class);
+    }
+
+}
